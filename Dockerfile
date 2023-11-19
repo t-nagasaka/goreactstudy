134c45304569d3846a099177f01835b839da7758
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2023-11-19 09:02:20 +0000

    デバッグ方法の追加

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,15 @@ RUN apk update && apk add --no-cache git
 RUN apk add bash
 RUN apk add openssh-client
 
+# ロケールの設定
+RUN apk add --no-cache libc6-compat
+
+
 # ワーキングディレクトリの設定
 WORKDIR /myapp
 RUN go install -v golang.org/x/tools/gopls@latest
+RUN go install github.com/go-delve/delve/cmd/dlv@latest
+
 
 # コンテナ内でSSHエージェントを使えるように
 CMD ["sh", "-c", "eval $(ssh-agent -s) && /bin/sh"]
--- db/db.go
+++ db/db.go
@@ -1,10 +1,12 @@
 package db
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -12,7 +14,22 @@ func NewDB() *gorm.DB {
 	if os.Getenv("GO_ENV") == "dev" {
 		err := godotenv.Load()
 		if err != nil {
-			log.Fatalln(err)
+			log.Println("Error loading .env file:", err)
 		}
+	}
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Println("Error connecting to the database:", err)
+		return nil
+	}
+	fmt.Println("Connected")
+	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Println("Error closing the database connection:", err)
 	}
 }
